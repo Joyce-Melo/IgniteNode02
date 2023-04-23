15a48c8221b307c107e9c3c30bf162481608e1aa
Joyce Melo <joyce.santosm@hotmail.com>
2023-04-23 13:44:49 -0300

    Feat: List all transactions, get a specific transaction and get the summary tests added

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,12 @@
-import "dotenv/config";
+// import "dotenv/config"; //Dessa forma ele importará única e exclusivamento o .env, porém nós queremos o .env.test também, enrão faremos o seguinte
+import { config } from "dotenv";
 import { z } from "zod";
+
+if (process.env.NODE_ENV === 'test'){ //quando executamos npm run test, o vitest já preenche automáticamento o NODE_ENV como test
+  config({path:'.env.test'}) //Caso seja o teste, estamos passando para o config que o arquivo de variavel de ambiente que ele precisa executar é o .env.test
+} else { //caso não o ambiente não seja o de teste
+  config() //executamos o cofig direto (lembrando que o config() procura pelo arquivo .env por padrão)
+}
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
